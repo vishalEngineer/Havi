00708c347d410a5a5de9dfd988726448fbea26da
Vishal <vishalgunna@gmail.com>
2020-09-23 20:36:07 +0530

    new edit

--- log.php
+++ log.php
@@ -16,8 +16,20 @@ if(mysqli_connect_errno())
             }
             else
             {
-                echo "not registered";
-            header('Location:signup.html');
+                $querytwo = "select username,password from signupdata where username='$username' and password!='$password'";
+                //echo $querytwo;
+                $selq = mysqli_query($con,$querytwo);
+                if(mysqli_num_rows($selq)!=0)
+                {
+                    //echo "Password Mismatch";
+                    echo "<script>alert('invalid credentials')</script>";  
+
+                }
+                else{
+                    echo "not registered";
+                    header('Location:signup.html');
+                }
+                
             }
         }  
       else{
